866817473c6dae45b5a8174a827fa478c1f40a46
ju1ien1100 <julien.roy22@outlook.com>
2024-01-31 22:11:16 -0800

    First commit

---
+++ Source/SoundStudios/SoundStudiosGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SoundStudiosGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOUNDSTUDIOS_API ASoundStudiosGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
